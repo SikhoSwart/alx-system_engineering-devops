7def797f1caa73df1833403c0e2eca661bbc3336
SikhoSwart <sikhoswart@gmail.com>
2023-12-12 12:23:58 -0800

    Task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for task 2"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts"""
+    sub = requests.get("https://www.reddit.com/r/{}/hot.json"
+                       .format(subreddit),
+                       params={"count": count, "after": after},
+                       headers={"User-Agent": "My-User-Agent"},
+                       allow_redirects=False)
+    if sub.status_code >= 400:
+        return None
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
+
+    info = sub.json()
+    if not info.get("data").get("after"):
+        return hot_l
+
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
